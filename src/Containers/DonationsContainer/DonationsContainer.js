c9e0570a34888814f3ffc2b047a8f92fc8c81a3f
Jon O'Drobinak <jodrobinak@gmail.com>
2019-11-07 11:15:03 -0700

    Update donations container, mapDispatch

--- src/Containers/DonationsContainer/DonationsContainer.js
+++ src/Containers/DonationsContainer/DonationsContainer.js
@@ -2,27 +2,32 @@ import React from 'react';
 import { connect } from 'react-redux';
 import './DonationContainer.scss';
 import { bindActionCreators } from 'redux';
+import { fetchDonations } from '../../util/apiCalls';
+
 
 export class DonationsContainer extends React.Component {
   state = {
     name: "",
     donation: ""
   };
-
   
-  fetchDonations = async () => {
+  getDonations = async () => {
     const donationData = {
       id: Date.now(),
       name: this.state.name,
       donation: this.state.donation
     };
-    
+    fetchDonations(donationData)
+  }
     handleSubmit = e => {
       e.preventDefault();
-      this.postDonor();
+      this.getDonations();
     };
 
-
+    getDonationData = (e) => {
+      let { name, value } = e.target;
+      this.setState({ [name]: value });
+    }
   };
 
   render() {
@@ -33,27 +38,27 @@ export class DonationsContainer extends React.Component {
             type="text"
             name="Name"
             value={this.state.name}
-            onChange={this.collectInputs}
+            onChange={this.getDonationData}
             autoComplete="off"
           />
           <input
             type="text"
             name="donation"
             value={this.state.donation}
-            onChange={this.collectDonations}
+            onChange={this.getDonationData}
           />
           <button className="button" onClick={this.handleSubmit}>
-            Donate!
+            Submit Donation
           </button>
         </form>
       </section>
     );
   }
-}
+
 
 export const mapDispatchToProps = dispatch => (
   bindActionCreators({
-    updateDonations,
+    getDonations,
     isLoading,
     hasErrored
   }, dispatch)
