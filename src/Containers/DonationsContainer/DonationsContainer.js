c970d61a44c4edcb8720a898661c9e093273dfcd
Jon O'Drobinak <jodrobinak@gmail.com>
2019-11-07 10:48:19 -0700

    Update donations container, apiCalls

--- src/Containers/DonationsContainer/DonationsContainer.js
+++ src/Containers/DonationsContainer/DonationsContainer.js
@@ -1,70 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import * as actions from "../../actions";
-import "./Inputs.css";
+import './DonationContainer.scss';
+import { bindActionCreators } from 'redux';
 
-class Inputs extends React.Component {
-  state = {
-    donorName: "",
-    donation: ""
-  };
-
-  collectInputs = e => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    this.postDonor();
-  };
-
-  postDonor = async () => {
-    const donorInfo = {
-      id: Date.now(),
-      name: this.state.donorName,
-      donation: this.state.donation
-    };
-    const response = await fetch("http://localhost:3001/api/v1/donations/", {
-      method: "POST",
-      headers: { "content-type": "application/json" },
-      body: JSON.stringify(donorInfo)
-    });
-    this.props.updateDonations(await response.json());
-  };
-
-  render() {
-    return (
-      <section className="Inputs">
-        <form>
-          <input
-            type="text"
-            name="donorName"
-            value={this.state.donorName}
-            onChange={this.collectInputs}
-            autoComplete="off"
-          />
-          <input
-            type="text"
-            name="donation"
-            value={this.state.donation}
-            onChange={this.collectInputs}
-            autoComplete="off"
-          />
-          <button className="main-btn" onClick={this.handleSubmit}>
-            Donate!
-          </button>
-        </form>
-      </section>
-    );
-  }
+export class DonationsContainer extends React.Component {
+ 
 }
 
-const mapDispatchToProps = dispatch => ({
-  updateDonations: donation => dispatch(actions.updateDonations(donation))
-});
+export const mapDispatchToProps = dispatch => (
+  bindActionCreators({
+    updateDonations,
+    isLoading,
+    hasErrored
+  }, dispatch)
+);
+
 
 export default connect(
   null,
   mapDispatchToProps
-)(Inputs);
+)(DonationsContainer);
