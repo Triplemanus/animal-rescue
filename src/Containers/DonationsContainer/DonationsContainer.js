f9f1675de02d85aa41b8b7ca4dee9d7e77f23023
Jon O'Drobinak <jodrobinak@gmail.com>
2019-11-07 11:03:22 -0700

    Add donations api, update donations container

--- src/Containers/DonationsContainer/DonationsContainer.js
+++ src/Containers/DonationsContainer/DonationsContainer.js
@@ -4,7 +4,51 @@ import './DonationContainer.scss';
 import { bindActionCreators } from 'redux';
 
 export class DonationsContainer extends React.Component {
- 
+  state = {
+    name: "",
+    donation: ""
+  };
+
+  
+  fetchDonations = async () => {
+    const donationData = {
+      id: Date.now(),
+      name: this.state.name,
+      donation: this.state.donation
+    };
+    
+    handleSubmit = e => {
+      e.preventDefault();
+      this.postDonor();
+    };
+
+
+  };
+
+  render() {
+    return (
+      <section className="donations">
+        <form>
+          <input
+            type="text"
+            name="Name"
+            value={this.state.name}
+            onChange={this.collectInputs}
+            autoComplete="off"
+          />
+          <input
+            type="text"
+            name="donation"
+            value={this.state.donation}
+            onChange={this.collectDonations}
+          />
+          <button className="button" onClick={this.handleSubmit}>
+            Donate!
+          </button>
+        </form>
+      </section>
+    );
+  }
 }
 
 export const mapDispatchToProps = dispatch => (
--- src/util/apiCalls.js
+++ src/util/apiCalls.js
@@ -1,15 +1,21 @@
 export const fetchAnimals = async (animals) => {
   const url = `http://localhost:3001/api/v1/${animals}`;
 
-  //   const response = await fetch(url);
-  //   return await response.json();
-  // } catch (error) {
-  //   throw new Error(error.message);
-  // }
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error ('There was an error getting animals data.');
   }
   const animals = await response.json();
   return animals;
+};
+
+export const fetchDonations = async (donations) => {
+  const url = "http://localhost:3001/api/v1/donations/";
+
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error ('There was an error getting donations data.');
+  }
+  const donations = await response.json();
+  return donations;
 }
